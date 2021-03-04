6d8889c50b61188d604d18c41d63a22fde4a903b
Daniel Y <7935418+DanDankRaft@users.noreply.github.com>
2021-03-03 23:21:46 -0600

    refactored highlights, fixed logo also added color pallette

---
+++ darkmode.js
@@ -0,0 +1,14 @@
+function darkmodestartup()
+{
+    var body = document.body;
+    body.classList.toggle("lightmode");
+}
+
+//toggle dark mode through a button
+function darkMode()
+{
+    var body = document.body;
+    body.classList.toggle("darkmode");
+    body.classList.toggle("lightmode");
+    
+}
